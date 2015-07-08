12f4d27279a9fc2c10a30cc1fe0ad6dac9d8f052
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-07-08 19:35:09 +0100

    tidied the team scenarios

--- scenarios/9-team-all.rb
+++ scenarios/9-team-all.rb
@@ -3,9 +3,18 @@ require_relative '../lib/game'
 game = Game.new
 
 game.add_faction('red','red')
+game.add_faction('yellow','yellow')
+game.add_faction('green','green')
+game.add_faction('teal','teal')
 game.add_faction('blue','blue')
+game.add_faction('purple','purple')
+
+game.add_faction('grey','grey')
+game.add_faction('white','white')
+game.add_faction('black','black')
 
 game.factions.each do |faction|
+  faction.hide_info_bar = true
   x = rand(X_SIZE)
   faction.add_unit(:peasant, x, rand(Y_SIZE))
   faction.add_unit(:coward_peasant, x, rand(Y_SIZE))
