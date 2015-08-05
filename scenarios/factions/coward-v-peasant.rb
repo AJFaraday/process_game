40c40a2cb249e38eac2c4e0adacbe51a8fb6220f
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-08-05 18:37:24 +0100

    Correctly set up coward v peasant scenario

--- scenarios/factions/coward-v-peasant.rb
+++ scenarios/factions/coward-v-peasant.rb
@@ -4,12 +4,12 @@ game = Game.new
 
 faction = game.add_faction('blue','blue')
 
-faction.add_unit(:peasant, 100, 240)
+faction.add_unit(:coward_peasant, 100, 240)
 faction.add_unit(:coward_peasant, 100, 340)
 
 faction = game.add_faction('red','red')
 
-faction.add_unit(:coward_peasant, 400, 240)
+faction.add_unit(:peasant, 400, 240)
 faction.add_unit(:peasant, 400, 340)
 
 game.factions.each do |faction|
